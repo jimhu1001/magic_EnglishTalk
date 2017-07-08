cee06ca481a7d422852e78a4374742c996198f9c
jimhu <jimhu1001@hotmail.com>
2017-07-08 09:28:01 +0800

    add

---
+++ englishTalk-user/englishTalk-user-service/src/main/java/com/english/rockGod/user/service/dao/ExampleDao.java
@@ -0,0 +1,11 @@
+package com.english.rockGod.user.service.dao;
+
+import com.english.rockGod.admin.service.util.BaseDao;
+import com.english.rockGod.user.service.entity.TestEntity;
+
+/**
+ * Created by Administrator on 2017/7/7/007.
+ */
+public interface ExampleDao extends BaseDao<TestEntity> {
+
+}
